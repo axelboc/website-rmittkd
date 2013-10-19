54b6aed48e164852ddcae56008c3e8d260d803fb
axelboc <axelboc@gmail.com>
2013-10-19 14:16:31 +1100

    Favicon

--- src/includes/head_css.php
+++ src/includes/head_css.php
@@ -1,3 +1,4 @@
+<link rel="shortcut icon" href="favicon.ico">
 <link rel="stylesheet" href="css/lib/normalize.css">
 <link rel="stylesheet" href="css/base.css">
 <link rel="stylesheet" href="css/modules.css">
