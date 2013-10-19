74ef10b0f6d4706c3ddaf776c9d41bae7b2110c9
axelboc <axelboc@gmail.com>
2013-10-19 12:53:37 +1100

    dist and node_modules folders removed from index

--- dist/includes/head_css.php
+++
@@ -1,3 +0,0 @@
-<link rel="stylesheet" href="css/lib/normalize.css">
-<link rel="stylesheet" href="css/base.css">
-<link rel="stylesheet" href="css/modules.css">
--- dist/includes/head_js.php
+++
@@ -1,2 +0,0 @@
-<script src="js/lib/html5shiv.min.js"></script>
-<script src="js/lib/respond.min.js"></script>
