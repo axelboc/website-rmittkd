be0b0f458637a070c773d64e8b24de717f19f203
axelboc <axelboc@gmail.com>
2013-11-10 12:46:37 +1100

    New FAQ question

--- src/contact.php
+++ src/contact.php
@@ -64,13 +64,15 @@
 							<dt class="faq-q">What exactly do you do at training?</dt>
 								<dd class="faq-a">We always start with a good warm-up and finish with stretching. The rest is usually a mix of fitness and strength training, and practice of techniques, patterns, sparring drills and self-defence.</dd>
 							<dt class="faq-q">What should I bring to training?</dt>
-								<dd class="faq-a">For your first classes, just come in <strong>gym clothes</strong> and bring a <strong>water bottle</strong>. That's all you need to get started!</dd>
+								<dd class="faq-a">For your first classes, just come in gym clothes and bring a water bottle. That's all you need to get started!</dd>
 							<dt class="faq-q">Don't I need a uniform?</dt>
 								<dd class="faq-a">You will eventually, but there is no rush. Just discuss it with us at training once you become a member. By the way, the Taekwon-Do uniform is called a <em>dobok</em>.</dd>
 							<dt class="faq-q">I've done martial arts before. Can I bring my own uniform and gear?</dt>
 								<dd class="faq-a">Sorry, but only ITF Taekwon-Do uniforms are allowed in class. Gloves and boots are provided by the club.</dd>
+							<dt class="faq-q">What time do classes start?</dt>
+								<dd class="faq-a">Class times are displayed on the homepage. However, if it is your first class, please come in 15 minutes before to introduce yourself and fill out some paperwork (contact details, next of kin, etc.)</dd>
 							<dt class="faq-q">What happens if I get injured during class?</dt>
-								<dd class="faq-a">All members (inc. non-RMIT) are covered by RMIT's insurance. You are also covered during your first 3 free classes &ndash; you'll just be asked to fill in a short form before training (contact details, next of kin, etc.)</dd>
+								<dd class="faq-a">All members (inc. non-RMIT) are covered by RMIT's insurance. You are also covered during your first 3 free classes.</dd>
 						</dl>
 					</section>
 		    	</div>
