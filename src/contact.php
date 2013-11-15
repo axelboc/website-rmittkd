fb3e530c560ea28f12198358d738f12764ed35a4
axelboc <axelboc@gmail.com>
2013-11-15 22:27:40 +1100

    Add link to homepage + copy tweak

--- src/contact.php
+++ src/contact.php
@@ -70,7 +70,7 @@
 							<dt class="faq-q">I've done martial arts before. Can I bring my own uniform and gear?</dt>
 								<dd class="faq-a">Sorry, but only ITF Taekwon-Do uniforms are allowed in class. Gloves and boots are provided by the club.</dd>
 							<dt class="faq-q">What time do classes start?</dt>
-								<dd class="faq-a">Class times are displayed on the homepage. However, if it is your first class, please come in 15 minutes before to introduce yourself and fill out some paperwork (contact details, next of kin, etc.)</dd>
+								<dd class="faq-a">Class times are displayed on the <a href="/" class="link-blend">homepage</a>. However, if it is your first class, please come in 15 minutes beforehand to introduce yourself and fill out some paperwork (contact details, next of kin, etc.)</dd>
 							<dt class="faq-q">What happens if I get injured during class?</dt>
 								<dd class="faq-a">All members (inc. non-RMIT) are covered by RMIT's insurance. You are also covered during your first 3 free classes.</dd>
 						</dl>
