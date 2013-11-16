d68a57769168261ceff03a25b185c9f8907110aa
axelboc <axelboc@gmail.com>
2013-11-16 14:48:50 +1100

    TKD page copy + CSS tweaks for images

--- src/tkd.php
+++ src/tkd.php
@@ -22,10 +22,10 @@
 				<h1 class="section-heading"><abbr title="International Taekwon-Do Federation">ITF</abbr> Taekwon-Do</h1>
 				<img class="itf-image hero img-mob-left img-tab-left respimg-fit lazy-loading" src="" data-src="images/hero/hero-tkd-suffix.png" width="400" height="502" alt="">
 				<div class="itf-content section-content">
-					<p>The founder of Taekwon-Do is <strong>Choi Hong Hi</strong> (1918-2002), a South Korean General and Ambassador.</p>
-					<p>Taekwon-Do is based on <strong>Tae-Kyon</strong>, a traditional Korean martial art that is over 1300 years old. General Choi developed the art whilst being a prisoner during World War II. He practiced it to keep up his morale and that of his fellow prisoners. On 16 August 1944, two days before he was due to be executed by his captors, General Choi and his friends were liberated by US forces.</p>
-					<p>The name <em><strong>Taekwon-Do</strong></em> was officially created on <strong>11 April 1955</strong>. As a member of the Korean military, General Choi taught Taekwon-Do to his soldiers. But after he left the military and became Korea's first Ambassador to Malaysia, he was able to teach Taekwon-Do internationally.</p>
-					<p>Because of its success, Taekwon-Do quickly found the need for an organisation to look over its growing community. Therefore, on March 1966, the <strong>International Taekwon-Do Federation (ITF)</strong> was founded, of which we are proud to be members today.</p>
+					<p>Taekwon-Do is a version of unarmed combat designed for the purpose of <strong>self-defence</strong>. It was founded in the 1950s by <strong>Choi Hong Hi</strong> (1918-2002), a South Korean General and Ambassador. Our club is part of the official International Taekwon-Do Federation (<strong>ITF</strong>), which was founded by General Choi in 1966.</p>
+					<p>Taekwon-Do martial artists employ both <strong>hand</strong> and <strong>foot techniques</strong>. Supported by modern scientific evidence, these techniques combine the notions of reaction force, concentration, balance, breath control and speed in order to achieve maximum power.</p>
+					<p>One of the many appeals of ITF Taekwon-Do is the diversity of events that take place during competitions. There is sparring of course, but also patterns, team sparring, team patterns, self-defence, power breaking and special techniques.</p>
+					<p>Ultimately, it's an excellent tool for both <strong>physical</strong> and <strong>mental development</strong>. But don't take our word for it... Come and try it out!</p>
 				</div>
 			</header>
 			<div class="row">
@@ -35,8 +35,8 @@
 						<img class="lai-image img-mob-left img-tab-left respimg-fit lazy-loading" src="" data-src="images/sm-lai-suffix.png" width="270" height="350" alt="">
 						<div class="lai-content section-content">
 							<p>The RMIT ITF Taekwon-Do Club is affiliated to Lai Taekwon-Do, under the guidance of <strong>Senior Master Robert Lai</strong> (8th degree) and his son Jason Lai (6th degree).</p>
-							<p>Senior Master Lai was born in Malaysia. He started Taekwon-Do in 1964 <strong>at the age of 19</strong>, after watching a demonstration by General Choi and his squad. The training hall was a small Buddhist temple so numbers were limited, and Senior Master Lai was one of 30 students chosen to begin training. According to Senior Master Lai, Taekwon-Do was the <em>in</em> thing to do.</p>
-							<p>Senior Master Lai achieved his black belt in 1966 and <strong>represented Malaysia</strong> in the early <strong>international tournaments</strong> of 1967, 1968 and 1971. Senior Master Lai continued to represent the state of Selangor until 1973, then decided to concentrate on instructing and coaching. In 10 years, he built up one of the largest number of clubs in Selangor, totalising more than a thousand members.</p>
+							<p>Senior Master Lai was born in Malaysia. He started Taekwon-Do in 1964 <strong>at the age of 19</strong>, after watching a demonstration by General Choi and his squad. The training hall was a small Buddhist temple so numbers were limited, and Senior Master Lai was one of 30 students chosen to begin training.</p>
+							<p>Senior Master Lai achieved his black belt in 1966 and <strong>represented Malaysia</strong> in the early international tournaments of 1967, 1968 and 1971. In the following years, he decided to focus on instructing, and built up many clubs around Malaysia. He then moved to Australia in the 1980s, where he became a <strong><abbr title="Physical Education">PE</abbr> teacher</strong> at St Albans Secondary College and founded Lai Taekwon-Do.</p>
 						</div>
 					</section>
 				</div>
