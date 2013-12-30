2d69013e3bba6ea77d3499e9f78ab2895aa003ec
axelboc <axelboc@gmail.com>
2013-12-30 17:57:53 +1100

    New Youtube box + layout changes on social page
    Links box drops next to Facebook box and is replaced with Youtube box.
    Youtube box has 2 videos and a subscribe button.
    Hero image is removed.

--- src/social.php
+++ src/social.php
@@ -18,45 +18,51 @@
 <div class="body-wrap">
 	<?php include 'includes/header.php'; ?>
 	<article class="main row" role="main">
-		<div class="body-max">
-			<img class="social-hero hero respimg-fit" src="images/hero/hero-social.png" width="331" height="303" alt="">
-		</div>
 		<div class="main-wrap body-max">
-			<header class="section box">
-				<h1 class="section-heading">Links</h1>
-				<div class="section-content row">
-					<section class="box">
-						<h2 class="section-heading-sub">International &amp; National Federations</h2>
-						<ul class="res-list list-reset links-inv links-blend row">
-							<li class="resource box"><a class="res-link" href="http://www.itftkd.org/" target="_blank"><img src="images/social/logo-itf.png" width="120" height="120" alt=""> International Taekwon-Do Federation (ITF)</a></li>
-							<li class="resource box"><a class="res-link" href="http://www.aaitf.org.au/" target="_blank"><img src="images/social/logo-aaitf.png" width="120" height="120" alt=""> All Australia International Taekwon-Do Federation (AAITF) Inc.</a></li>
-						</ul>
-					</section>
-					<section class="box">
-						<h2 class="section-heading-sub clubs-heading">Associated Lai Taekwon-Do Clubs</h2>
-						<ul class="res-group res-group--first box res-list list-reset links-blend row">
-							<li class="resource--min"><span class="res-link--min"><img src="images/social/logo-lai.png" width="40" height="40" alt="" /> <span>St Albans Lai Taekwon-Do (<abbr title="Victoria">VIC</abbr>)</span></span></li>
-							<li class="resource--min"><a class="res-link--min" href="http://www.budosites.com/gmaa/" target="_blank"><img src="images/social/logo-gmaa.png" width="40" height="40" alt=""> <span>Goulburn Martial Arts Academy (<abbr title="New South Wales">NSW</abbr>)</span></a></li>
-							<li class="resource--min"><a class="res-link--min" href="http://www.tkdsomerset.com/" target="_blank"><img src="images/social/logo-somerset.png" width="40" height="40" alt=""> <span>Somerset Lai Taekwon-Do (<abbr title="Tasmania">TAS</abbr>)</span></a></li>
-						</ul>
-						<ul class="res-group box res-list list-reset links-blend row">
-							<li class="resource--min"><a class="res-link--min" href="http://www.facebook.com/pages/Melton-Lai-Taekwon-Do-ITF/119160461466076" target="_blank"><img src="images/social/logo-lai.png" width="40" height="40" alt=""> <span>Melton Lai Taekwon-Do (<abbr title="Victoria">VIC</abbr>)</span></a></li>
-							<li class="resource--min"><a class="res-link--min" href="http://www.facebook.com/Laitkdtl" target="_blank"><img src="images/social/logo-lai.png" width="40" height="40" alt=""> <span>Taylors Lakes Lai Taekwon-Do (<abbr title="Victoria">VIC</abbr>)</span></a></li>
-						</ul>
-					</section>
-				</div>
-			</header>
+			<div class="row">
+				<section class="yt-section section box row">
+					<h2 class="section-heading">Youtube</h2>
+					<div class="yt-wrap section-content row">
+						<div class="yt-subscribe">
+							<div class="g-ytsubscribe" data-channelid="UCC6zmnN4NLQyvlFLfdr18Dw" data-layout="full" data-count="hidden"></div>
+						</div>
+						<div class="video video--first">
+							<iframe src="//www.youtube-nocookie.com/embed/YtSfWZdC_mE?rel=0&amp;wmode=transparent" frameborder="0" allowfullscreen></iframe>
+						</div>
+						<div class="video">
+							<iframe src="//www.youtube-nocookie.com/embed/H-lffvJshJo?rel=0&amp;wmode=transparent" frameborder="0" allowfullscreen></iframe>
+						</div>
+					</div>
+				</section>
+			</div>
 			<div class="row">
 				<div class="lay-main">
-					<!--<section class="section box row">
-						<h2 class="section-heading">Youtube</h2>
-						<div class="section-content">
+					<section class="section section-1of2 section-first box">
+						<h1 class="section-heading">Links</h1>
+						<div class="links-wrap section-content row">
+							<section class="box">
+								<h2 class="section-heading-sub">International &amp; National Federations</h2>
+								<ul class="list-reset links-inv links-blend row">
+									<li class="resource box"><a class="res-link" href="http://www.itftkd.org/" target="_blank"><img src="images/social/logo-itf.png" width="100" height="100" alt=""> International Taekwon-Do Federation</a></li>
+									<li class="resource box"><a class="res-link" href="http://www.aaitf.org.au/" target="_blank"><img src="images/social/logo-aaitf.png" width="100" height="100" alt=""> All Australia ITF<br>(AAITF)</a></li>
+								</ul>
+							</section>
+							<section class="box">
+								<h2 class="section-heading-sub clubs-heading">Associated Lai Taekwon-Do Clubs</h2>
+								<ul class="res-group list-reset links-inv links-blend row">
+									<li class="resource--min"><span class="res-link--min"><img src="images/social/logo-lai.png" width="40" height="40" alt="" /> <span>St Albans Lai Taekwon-Do (<abbr title="Victoria">VIC</abbr>)</span></span></li>
+									<li class="resource--min"><a class="res-link--min" href="http://www.facebook.com/pages/Melton-Lai-Taekwon-Do-ITF/119160461466076" target="_blank"><img src="images/social/logo-lai.png" width="40" height="40" alt=""> <span>Melton Lai Taekwon-Do (<abbr title="Victoria">VIC</abbr>)</span></a></li>
+									<li class="resource--min"><a class="res-link--min" href="http://www.facebook.com/Laitkdtl" target="_blank"><img src="images/social/logo-lai.png" width="40" height="40" alt=""> <span>Taylors Lakes Lai Taekwon-Do (<abbr title="Victoria">VIC</abbr>)</span></a></li>
+									<li class="resource--min"><a class="res-link--min" href="http://www.budosites.com/gmaa/" target="_blank"><img src="images/social/logo-gmaa.png" width="40" height="40" alt=""> <span>Goulburn Martial Arts Academy (<abbr title="New South Wales">NSW</abbr>)</span></a></li>
+									<li class="resource--min"><a class="res-link--min" href="http://www.tkdsomerset.com/" target="_blank"><img src="images/social/logo-somerset.png" width="40" height="40" alt=""> <span>Somerset Lai Taekwon-Do (<abbr title="Tasmania">TAS</abbr>)</span></a></li>
+								</ul>
+							</section>
 						</div>
-					</section>-->
-					<section class="section box row">
+					</section>
+					<section class="section section-1of2 box">
 						<h2 class="section-heading">Facebook</h2>
 						<div class="fb-wrap section-content">
-							<iframe class="fb-box" src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Frmittkd&amp;width=292&amp;height=395&amp;colorscheme=light&amp;show_faces=false&amp;header=false&amp;stream=true&amp;show_border=false"></iframe>
+							<iframe class="fb-box" src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Frmittkd&amp;width=292&amp;height=404&amp;colorscheme=light&amp;show_faces=false&amp;header=false&amp;stream=true&amp;show_border=false"></iframe>
 						</div>
 					</section>
 				</div>
@@ -66,6 +72,7 @@
 	</article>
 	<?php include 'includes/footer.php'; ?>
 	<?php include 'includes/bottom_js.php'; ?>
+	<script src="https://apis.google.com/js/platform.js"></script>
 </div>
 </body>
 </html>
