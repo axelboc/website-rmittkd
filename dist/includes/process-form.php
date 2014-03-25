233e3bce01e7452c5f1ef20030228216ded6aacf
axelboc <axelboc@gmail.com>
2014-03-25 22:45:29 +1100

    Version control dist folder for Heroku deployment

---
+++ dist/includes/process-form.php
@@ -0,0 +1,110 @@
+<?php
+
+define('TRY_AGAIN', 'Please try again. If the problem persists, please get in touch with us on Facebook.');
+
+function exitWithResult($status, $message = '') {
+	echo json_encode(array(
+		'status' => $status,
+		'message' => $message
+	));
+	exit;
+}
+
+
+/* ===== Name validation ===== */
+
+// Check presence
+if (!isset($_POST['name'])) {
+	exitWithResult('error', 'Name not provided. ' . TRY_AGAIN);
+}
+
+// Retrieve value
+$name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+
+// Check length
+if (strlen($name) === 0) {
+	exitWithResult('error', 'Please enter your name.');
+}
+
+
+/* ===== Email validation ===== */
+
+// Check presence
+if (!isset($_POST['email'])) {
+	exitWithResult('error', 'Email address not provided. ' . TRY_AGAIN);
+}
+
+// Retrieve value
+$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+
+// Check length
+if (strlen($email) === 0) {
+	exitWithResult('error', 'Please enter your email address.');
+}
+
+// Check validity
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+	exitWithResult('error', 'Please enter your email address.');
+}
+
+
+/* ===== Message validation ===== */
+
+// Check presence
+if (!isset($_POST['message'])) {
+	exitWithResult('error', 'Message not provided. ' . TRY_AGAIN);
+}
+
+// Retrieve value
+$message = strip_tags($_POST['message']);
+
+// Check length
+if (strlen($message) === 0) {
+	exitWithResult('error', 'Please enter your message.');
+}
+
+
+/* ===== Prepare and send email ===== */
+
+/**
+ * File includes/config.php must define two constants:
+ * - API_KEY, the Mandrill API key (https://mandrillapp.com/)
+ * - CLUB_EMAIL, the club's email address
+ */
+include('config.php');
+
+$emailBody = "A new message was posted on the website.\r\n\r\n" .
+			 "=== Name ===\r\n$name\r\n\r\n" .
+			 "=== Email ===\r\n$email\r\n\r\n" .
+			 "=== Message ===\r\n$message";
+
+$data = array(
+	"key" => API_KEY,
+	"message" => array(
+		"text" => $emailBody,
+		"subject" => "New message",
+		"from_email" => CLUB_EMAIL,
+		"from_name" => "Website",
+		"to" => array(
+			array(
+				"email" => CLUB_EMAIL,
+				"name" => "RMIT ITF Taekwon-Do",
+				"type" => "to"
+			)
+		),
+		"headers" => array(
+			"Reply-To" => ($name . ' <' . $email . '>')
+		)
+	)
+);
+
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+
+curl_setopt($curl, CURLOPT_URL, "https://mandrillapp.com/api/1.0/messages/send.json");
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+echo curl_exec($curl);
+
+?>
---
+++ dist/index.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html lang="en-AU" prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#">
+<head>
+	<title>RMIT ITF Taekwon-Do</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Come and meet a bunch of awesome people at the ITF Taekwon-Do club of RMIT University, Melbourne. Just drop by in gym cloths and enjoy 3 classes for free!">
+	<meta property="og:type" content="website">
+	<meta property="og:title" content="RMIT ITF Taekwon-Do">
+	<meta property="og:description" content="Come and meet a bunch of awesome people at the ITF Taekwon-Do club of RMIT University, Melbourne. Just drop by in gym cloths and enjoy 3 classes for free!">
+	<meta property="og:image" content="http://rmittkd.com/images/og/logo-og.jpg">
+	<meta property="og:url" content="http://rmittkd.com/">
+	<?php include 'includes/head_css.php'; ?>
+	<link rel="stylesheet" href="css/modules-index.css">
+	<?php include 'includes/head_js.php'; ?>
+</head>
+<body>
+<div class="body-wrap">
+	<?php include 'includes/header.php'; ?>
+	<article class="main row" role="main">
+		<div class="body-max">
+			<img class="index-hero hero respimg-fit lazy-loading" src="" data-src="images/hero/hero-index-suffix.jpg" width="825" height="610" alt="">
+		</div>
+		<div class="main-wrap body-max">
+			<div class="row">
+				<div class="lay-main lay-main--welcome">
+					<div class="row">
+						<header class="section section-1of2 box section--welcome row">
+				        	<h1 class="section-heading">RMIT ITF Taekwon-Do</h1>
+				        	<div class="section-content lh">
+				        		<p>Forget about the gym! Fitness, cardio, strength, self-defence... <strong>Taekwon-Do</strong> does it all! From complete beginner to seasoned martial artist, our club welcomes all levels of fitness and experience.</p>
+				        		<p>Enough to convince you? Have a look at the timetable below and join our next training session &ndash; you get <strong>3 classes for free!</strong> Otherwise, take a look around... We look forward to training with you!</p>
+				        	</div>
+						</header>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="lay-main">
+					<div class="row">
+						<section class="tmtbl-section section section-1of2 section-first box">
+							<h2 class="section-heading">Timetable</h2>
+							<div>
+								<table class="tmtbl" summary="The first colum gives the time, and the others each correspond to a day of the week.">
+									<caption class="tmtbl-caption"><span class="tmtbl-legend hidden-text">Legend: </span><span><span class="tmtbl-legend-square tmtbl-training tmtbl-top tmtbl-bot"></span> Training</span> <span><span class="tmtbl-legend-square tmtbl-openmat tmtbl-top tmtbl-bot"></span> Open mat</span></caption>
+									<thead>
+										<tr>
+											<th class="tmtbl-time" scope="col">Time</th>
+											<th scope="col">Mon</th>
+											<th scope="col">Wed</th>
+											<th scope="col">Fri</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr class="tmtbl-odd">
+											<th class="tmtbl-time" scope="row">18:00-18:30</th>
+											<td></td>
+											<td></td>
+											<td><span class="tmtbl-training tmtbl-top hidden-text">Training</span></td>
+										</tr>
+										<tr>
+											<th class="tmtbl-time" scope="row">18:30-19:00</th>
+											<td><span class="tmtbl-training tmtbl-top hidden-text">Training</span></td>
+											<td><span class="tmtbl-training tmtbl-top hidden-text">Training</span></td>
+											<td><span class="tmtbl-training hidden-text">Training</span></td>
+										</tr>
+										<tr class="tmtbl-odd">
+											<th class="tmtbl-time" scope="row">19:00-19:30</th>
+											<td><span class="tmtbl-training hidden-text">Training</span></td>
+											<td><span class="tmtbl-training hidden-text">Training</span></td>
+											<td><span class="tmtbl-training hidden-text">Training</span></td>
+										</tr>
+										<tr>
+											<th class="tmtbl-time" scope="row">19:30-20:00</th>
+											<td><span class="tmtbl-training tmtbl-bot hidden-text">Training</span></td>
+											<td><span class="tmtbl-training tmtbl-bot hidden-text">Training</span></td>
+											<td><span class="tmtbl-openmat hidden-text">Open mat</span></td>
+										</tr>
+										<tr class="tmtbl-odd">
+											<th class="tmtbl-time" scope="row">20:00-20:30</th>
+											<td></td>
+											<td></td>
+											<td><span class="tmtbl-openmat tmtbl-bot hidden-text">Open mat</span></td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</section>
+						<section class="section map-section section-1of2 box">
+							<h2 class="section-heading">Location</h2>
+							<div class="map-wrap inner">
+								<a class="map-link link-inv respimg-wrap" href="http://goo.gl/maps/tVKGc" target="_blank">
+									<img class="cal-img respimg-crop" src="images/map.png" width="473" height="149" alt="">
+									<span class="map-caption backdrop">RMIT Building 57, Level 4, 33 Lygon St</span>
+								</a>
+							</div>
+						</section>
+					</div>
+					<section class="section cal-section box">
+						<h2 class="section-heading">Calendar</h2>
+						<div class="section-content cal-wrapper">
+							<div id="calendar" class="calendar">
+								<a class="cal-arrow cal-arrow-prev hidden-text" href="#">Previous month</a>
+								<div class="cal-months-wrapper">
+									<div class="cal-months row">
+										<div class="cal-month cal-month-2013-4 box" tabindex="-1">
+											<div class="respimg-wrap">
+												<img class="cal-img respimg-crop" width="408" height="150" alt="" src="images/calendar/cal-camp2.jpg">
+											</div>
+											<div class="cal-caption backdrop">
+												<h3 class="cal-heading">April</h3>
+												<ul class="list-reset">
+													<li><strong>5</strong> Colour-belt grading</li>
+													<li><strong>12-14</strong> Training Camp</li>
+													<li><strong>20</strong> TAS Champs</li>
+												</ul>
+											</div>
+										</div>
+										<div class="cal-month cal-month-2013-5 box" tabindex="-1">
+											<div class="respimg-wrap">
+												<img class="cal-img respimg-crop" width="408" height="150" alt="" src="images/calendar/cal-imag10.jpg">
+											</div>
+											<div class="cal-caption backdrop">
+												<h3 class="cal-heading">May</h3>
+												<ul class="list-reset">
+													<li><strong>9-12</strong> International Martial Arts Games (IMAG)</li>
+													<li><strong>26</strong> Go kart</li>
+												</ul>
+											</div>
+										</div>
+										<div class="cal-month cal-month-2013-6 box" tabindex="-1">
+											<div class="respimg-wrap">
+												<img class="cal-img respimg-crop" width="408" height="150" alt="" src="images/calendar/cal-soccer.jpg">
+											</div>
+											<div class="cal-caption backdrop">
+												<h3 class="cal-heading">June</h3>
+												<ul class="list-reset">
+													<li><strong>15</strong> Indoor soccer</li>
+												</ul>
+											</div>
+										</div>
+										<div class="cal-month cal-month-2013-7 box" tabindex="-1">
+											<div class="respimg-wrap">
+												<img class="cal-img respimg-crop" width="408" height="150" alt="" src="images/calendar/cal-clubday4.jpg">
+											</div>
+											<div class="cal-caption backdrop">
+												<h3 class="cal-heading">July</h3>
+												<ul class="list-reset">
+													<li><strong>7</strong> Black-belt grading</li>
+													<li><strong>23-25</strong> RMIT orientation day + demo</li>
+												</ul>
+											</div>
+										</div>
+										<div class="cal-month cal-month-2013-8 box" tabindex="-1">
+											<div class="respimg-wrap">
+												<img class="cal-img respimg-crop" width="408" height="150" alt="" src="images/calendar/cal-champs3.jpg">
+											</div>
+											<div class="cal-caption backdrop">
+												<h3 class="cal-heading">August</h3>
+												<ul class="list-reset">
+													<li><strong>2</strong> Colour-belt grading</li>
+													<li><strong>3</strong> Bowling</li>
+													<li><strong>17</strong> RMIT ITF Championship</li>
+												</ul>
+											</div>
+										</div>
+										<div class="cal-month cal-month-2013-9 box" tabindex="-1">
+											<div class="respimg-wrap">
+												<img class="cal-img respimg-crop" width="408" height="150" alt="" src="images/calendar/cal-champs1.jpg">
+											</div>
+											<div class="cal-caption backdrop">
+												<h3 class="cal-heading">September</h3>
+												<ul class="list-reset">
+													<li><strong>21</strong> VIC Champs</li>
+												</ul>
+											</div>
+										</div>
+										<div class="cal-month cal-month-2013-10 box" tabindex="-1">
+											<div class="respimg-wrap">
+												<img class="cal-img respimg-crop" width="408" height="150" alt="" src="images/calendar/cal-placeholder.jpg">
+											</div>
+											<div class="cal-caption backdrop">
+												<h3 class="cal-heading">October</h3>
+												<ul class="list-reset">
+													<li><strong>5</strong> Annual General Meeting</li>
+													<li><strong>18</strong> Movie Night</li>
+												</ul>
+											</div>
+										</div>
+										<div class="cal-month cal-month-2013-11 box" tabindex="-1">
+											<div class="respimg-wrap">
+												<img class="cal-img respimg-crop" width="408" height="150" alt="" src="images/calendar/cal-dinner.jpg">
+											</div>
+											<div class="cal-caption backdrop">
+												<h3 class="cal-heading">November</h3>
+												<ul class="list-reset">
+													<li><strong>16</strong> Annual dinner</li>
+													<li><strong>30-1</strong> Nationals in Waga Waga</li>
+												</ul>
+											</div>
+										</div>
+										<div class="cal-month cal-month-2013-12 box" tabindex="-1">
+											<div class="respimg-wrap">
+												<img class="cal-img respimg-crop" width="408" height="150" alt="" src="images/calendar/cal-grading4.jpg">
+											</div>
+											<div class="cal-caption backdrop">
+												<h3 class="cal-heading">December</h3>
+												<ul class="list-reset">
+													<li><strong>30-1</strong> Nationals in Wagga Wagga</li>
+													<li><strong>6</strong> Colour-belt grading</li>
+													<li><strong>15</strong> Black-belt Grading</li>
+												</ul>
+											</div>
+										</div>
+										<div class="cal-month cal-month-2014-1 box" tabindex="-1">
+											<div class="respimg-wrap">
+												<img class="cal-img respimg-crop" width="408" height="150" alt="" src="images/calendar/cal-barbecue.jpg">
+											</div>
+											<div class="cal-caption backdrop">
+												<h3 class="cal-heading">January</h3>
+												<ul class="list-reset">
+													<li><strong>20</strong> Classes start</li>
+													<li><strong>26</strong> Club barbecue</li>
+													<li><strong>27</strong> No class</li>
+												</ul>
+											</div>
+										</div>
+										<div class="cal-month cal-month-2014-2 box" tabindex="-1">
+											<div class="respimg-wrap">
+												<img class="cal-img respimg-crop" width="408" height="150" alt="" src="images/calendar/cal-clubday4.jpg">
+											</div>
+											<div class="cal-caption backdrop">
+												<h3 class="cal-heading">February</h3>
+												<ul class="list-reset">
+													<li><strong>RMIT Clubs Days:</strong></li>
+													<li><strong>25</strong> Brunswick</li>
+													<li><strong>26</strong> Bundoora</li>
+													<li><strong>27</strong> City (demo)</li>
+												</ul>
+											</div>
+										</div>
+										<div class="cal-month cal-month-2014-3 box" tabindex="-1">
+											<div class="respimg-wrap">
+												<img class="cal-img respimg-crop" width="408" height="150" alt="" src="images/calendar/cal-clubday1.jpg">
+											</div>
+											<div class="cal-caption backdrop">
+												<h3 class="cal-heading">March</h3>
+												<ul class="list-reset">
+													<li><strong>5</strong> RMIT Activity Day</li>
+													<li><strong>6</strong> RMIT Clubs Day</li>
+												</ul>
+											</div>
+										</div>
+									</div>
+								</div>
+								<a class="cal-arrow cal-arrow-next hidden-text" href="#">Following month</a>
+							</div>
+						</div>
+					</section>
+				</div>
+		    	<?php include 'includes/sidebar.php'; ?>
+		    </div>
+		</div>
+	</article>
+	<?php include 'includes/footer.php'; ?>
+	<?php include 'includes/bottom_js.php'; ?>
+	<script src="js/calendar.js"></script>
+</div>
+</body>
+</html>
