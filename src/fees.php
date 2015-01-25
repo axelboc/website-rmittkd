83ee0f725e717be4b187fdaef59562993cde5b33
axelboc <axelboc@gmail.com>
2015-01-25 13:44:34 +1100

    copy: fees and calendar

--- src/fees.php
+++ src/fees.php
@@ -11,7 +11,7 @@
 	<meta property="og:url" content="http://rmittkd.com/fees">
 	<meta property="og:site_name" content="RMIT ITF Taekwon-Do">
 	<?php include 'includes/header_css.php'; ?>
-	<link rel="stylesheet" href="css/modules-fees.css">
+	<link rel="stylesheet" href="css/modules-fees.css?20150125">
 	<?php include 'includes/header_js.php'; ?>
 </head>
 <body>
@@ -19,40 +19,46 @@
 	<?php include 'includes/header.php'; ?>
 	<article class="main row" role="main">
 		<div class="body-max">
-			<img id="fees-hero" class="fees-hero hero respimg-fit lazy-loading" src="" data-src="images/hero/hero-fees-suffix.png" width="384" height="311" alt="">
+			<img id="fees-hero" class="fees-hero hero respimg-fit lazy-loading" src="" data-src="images/hero/hero-fees-suffix--rmit.png" width="384" height="311" alt="">
 		</div>
 		<div class="main-wrap body-max">
 			<header class="section box">
-				<h1 class="section-heading">2014 Membership</h1>
+				<h1 class="section-heading">2015 Full Membership</h1>
 				<div class="membership section-content row">
 					<ul class="mbrship-options list-reset row links-blend">
 						<li class="mbrship-opt">
-							<a href="https://rmitlink.rmit.edu.au/Store/Product.aspx?PID=3882" class="mbrship-opt-link row" title="Purchase it on RMIT Link (new window)" target="_blank">
-								<span class="mbrship-fee fee"><span class="fee-val">$60</span> <span class="fee-period">per year</span></span>
+							<a href="https://rmitlink.rmit.edu.au/Store/Product.aspx?PID=6148" class="mbrship-opt-link row" title="Purchase it on RMIT Link (new window)" target="_blank">
+								<span class="mbrship-fee fee"><span class="fee-val">$330</span> <span class="fee-period">per year</span></span>
 								<span class="mbrship-group"><span>RMIT</span> student</span>
 							</a>
 						</li>
 						<li class="mbrship-opt">
-							<a id="fees-public" href="https://rmitlink.rmit.edu.au/Store/Product.aspx?PID=3883" class="mbrship-opt-link row" title="Purchase it on RMIT Link (new window)" target="_blank">
-								<span class="mbrship-fee fee"><span class="fee-val">$110</span> <span class="fee-period">per year</span></span>
+							<a id="fees-public" href="https://rmitlink.rmit.edu.au/Store/Product.aspx?PID=6150" class="mbrship-opt-link row" title="Purchase it on RMIT Link (new window)" target="_blank">
+								<span class="mbrship-fee fee"><span class="fee-val">$380</span> <span class="fee-period">per year</span></span>
 								<span class="mbrship-group"><span>Public</span> non-RMIT</span>
 							</a>
 						</li>
 					</ul>
-					<p class="mbrship-note">To purchase your membership, you will need to <a href="https://rmitlink.rmit.edu.au/Common/Register.aspx?R=1" class="link-blend" target="_blank">register to RMIT Link</a>.</p>
+					<div class="mbrship-note">
+						<p><strong>Training fees included!</strong> <a class="link-blend" href="https://rmitlink.rmit.edu.au/Clubs/Club.aspx?CID=30" target="_blank">Half-year options</a> available.</p>
+						<p><a href="https://rmitlink.rmit.edu.au/Common/Register.aspx?R=1" class="link-blend" target="_blank">Register to RMIT Link</a> to purchase your membership.</p>
+					</div>
 				</div>
 			</header>
 			<div class="row">
 				<div class="lay-main">
 					<section class="section box">
-						<h1 class="section-heading">Training Fees</h1>
+						<h1 class="section-heading">Casual Membership</h1>
+						<div class="section-content lh">
+				        	<p>Not planning on coming to training very often? Get a <strong>casual membership</strong> (RMIT student: $60 - public/non-RMIT: $110) and pay for your classes <strong>as you go</strong>!</p>
+						</div>
 						<div class="lessons section-content row">
 							<img class="lessons-cards-img lazy-loading" src="" data-src="images/training-cards.png" width="250" height="210" alt="">
 							<div class="lessons-fee row">
 								<p class="lessons-each fee"><span class="fee-val">just $3</span> <span class="fee-period">per class</span></p>
 								<p class="lessons-month fee"><span class="fee-val">or $25</span> <span class="fee-period">per month</span></p>
 							</div>
-							<p class="lessons-3free"><span class="lessons-free">Free</span> <span class="lessons-3">first 3 classes</span></p>
+							<p class="lessons-3free"><span class="lessons-3">first 3 classes</span> <span class="lessons-free">Free</span></p>
 				            <p class="lessons-note">To purchase a training card or pay your monthly training fee, <a href="https://rmitlink.rmit.edu.au/Clubs/Club.aspx?CID=30" class="link-blend" target="_blank">visit RMIT Link</a></p>
 						</div>
 					</section>
