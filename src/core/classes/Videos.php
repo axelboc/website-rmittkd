a252229f5faf6308c7419990e040e1ab0107a17d
axelboc <axelboc@gmail.com>
2015-04-04 17:37:23 +1100

    feat(admin): server-side validation

--- src/core/classes/Videos.php
+++ src/core/classes/Videos.php
@@ -24,18 +24,27 @@ class Videos extends Feature {
 	
 	/**
 	 * Set the URLs of the videos and persist the changes to the store.
-	 * @param {Array} $urls
+	 * @param {Array} $data
 	 */
-	public static function setUrls($urls) {
+	public static function update($data, &$errors) {
 		$videos = self::getInstance()->xml->video;
+		$i = 0;
 		
-		foreach ($urls as $index => $url) {
+		foreach ($data as $field => $url) {
 			// Look for the ID of the video in the URL
 			$matches = array();
 			preg_match(self::$urlRegex, $url, $matches);
 			
-			$videos[$index] = $url;
-			$videos[$index]['id'] = $matches[1];
+			// Ensure that the URL is well formed
+			if (count($matches) < 2) {
+				$errors[] = [$field, 'malformed'];
+				return;
+			}
+			
+			// Store video URL and ID
+			$videos[$i] = $url;
+			$videos[$i]['id'] = $matches[1];
+			$i++;
 		}
 		
 		self::getInstance()->persist();
--- src/core/core.php
+++ src/core/core.php
@@ -18,9 +18,75 @@ define('PATH_DATA', PATH_ROOT . 'data/');
 // Configuration
 require_once PATH_CORE . 'config.php';
 
+// Logging
+ini_set('log_errors', 1);
+ini_set('error_log', PATH_ROOT . 'data/error.log');
+
 // Auto-load feature classes
 function __autoload($class_name) {
     require_once PATH_CORE . 'classes/' . $class_name . '.php';
+}
+
+
+/**
+ * Sanitise and validate a POST parameter.
+ * @param {Array} &$data - the array in which to store the parameter's value after processing
+ * @param {Array} &$errors
+ * @param {String} $param - the name of the parameter
+ * @param {String} $type - the parameter's type, used for sanitisation and validation
+ * @param {Boolean} $isRequired - whether the parameter's value cannot be empty
+ * @param {Boolean} $validate - whether to validate the parameter's value
+ */
+function validatePOSTParam(&$data, &$errors, $param, $type = 'text', $isRequired = true, $validate = true) {
+	// Ensure that the parameter is set
+	if (!isset($_POST[$param])) {
+		$errors[] = 'Unexpected error.';
+		error_log('[core] POST parameter not set: ' . $param);
+		return;
+	}
+
+	// Determine sanitisation filter based on type
+	switch ($type) {
+		case 'url':
+			$filter = FILTER_SANITIZE_URL;
+			break;
+		default:
+			$errors[] = 'Unexpected error.';
+			error_log('[core] unkown POST parameter type: ' . $type);
+			return;
+	}
+
+	// Sanitise
+	$value = filter_var($_POST[$param], $filter);
+
+	// If required parameter, ensure that a value is provided
+	if ($isRequired) {
+		if ($value === '') {
+			$errors[] = [$param, 'not-provided'];
+			return;
+		}
+	}
+
+	// If requested, validate the value according to the parameter's type
+	if ($validate) {
+		switch ($type) {
+			case 'url':
+				$filter = FILTER_VALIDATE_URL;
+				break;
+			default:
+				$errors[] = 'Unexpected error.';
+				error_log('[core] unkown POST parameter type: ' . $type);
+				return;
+		}
+
+		// Validate
+		if (filter_var($value, $filter) === false) {
+			$errors[] = [$param, 'invalid'];
+			return;
+		}
+	}
+	
+	$data[$param] = $value;
 }
 
 
