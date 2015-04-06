fb16dbb8ea1b196eee40cd4d061ef9870ac5399c
axelboc <axelboc@gmail.com>
2015-04-06 16:10:41 +1000

    chore

--- src/core/config.php
+++ src/core/config.php
@@ -3,10 +3,11 @@
 // The Mandrill API key (https://mandrillapp.com/)
 define('API_KEY', 'RPlGTUmSdNX7puX-euw4Ag');
 
-// The club's email address
+// The club's email address and an address to CC the emails to for testing (website administrator)
 define('CLUB_EMAIL', 'itftaekwondo.club@rmit.edu.au');
+define('BCC_EMAIL', 'axelboc@gmail.com');
 
 // The administrator password
-define('ADMIN_PWD', 'admin');
+define('ADMIN_PWD', 'RMIT ITF TKD is the best club ever');
 
 ?>
