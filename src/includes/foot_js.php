2c4b1c94375d0dc3f6b35fac1e9f1514171fd926
axelboc <axelboc@gmail.com>
2015-05-02 16:46:31 +1000

    chore: improve cache busting process
    Replace `grunt-useref` with `grunt-dom-munger` and `grunt-filerev` to
    append an MD5 hash to the filenames of the CSS and JS bundles during
    build. This avoids the files to be renamed when their content hasn't
    changed.

--- src/includes/foot_js.php
+++ src/includes/foot_js.php
@@ -1,7 +1,5 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<!-- build:js /js/main-<%= grunt.template.today('yymmdd') %>.min.js -->
 <script src="/js/core.js"></script>
 <script src="/js/calendar.js"></script>
 <script src="/js/instructors.js"></script>
 <script src="/js/contact.js"></script>
-<!-- endbuild -->
--- src/includes/head_css.php
+++ src/includes/head_css.php
@@ -1,7 +1,6 @@
 <meta property="og:locale" content="en_GB">
 <link rel="shortcut icon" href="favicon.ico">
 <link rel="publisher" href="https://www.google.com/+RmittkdCHITF">
-<!-- build:css /css/main-<%= grunt.template.today('yymmdd') %>.min.css -->
 <link rel="stylesheet" href="/css/lib/normalize.css">
 <link rel="stylesheet" href="/css/base.css">
 <link rel="stylesheet" href="/css/modules.css">
@@ -11,4 +10,3 @@
 <link rel="stylesheet" href="/css/page-index.css">
 <link rel="stylesheet" href="/css/page-social.css">
 <link rel="stylesheet" href="/css/page-tkd.css">
-<!-- endbuild -->
