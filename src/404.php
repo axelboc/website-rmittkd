4a559ce3120ce54a5e5226ff4e7726745804ae2a
axelboc <axelboc@gmail.com>
2015-05-04 19:27:50 +1000

    content: 404 text

--- src/404.php
+++ src/404.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
-<html lang="en-AU">
+<html lang="en-AU" prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#">
 <head>
 	<title>404 | RMIT ITF Taekwon-Do</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta property="og:image" content="http://rmittkd.com/images/og/logo-og.png">
 	<?php include 'includes/head_css.php'; ?>
 	<?php include 'includes/head_js.php'; ?>
 	<script>
@@ -23,7 +24,7 @@
 					<section class="section row">
 						<h1 class="section-heading">Page Not Found</h1>
 						<div class="section-content lh">
-							<p><em>Ouch!</em> The board didn't break. In normal circumstances, we'd recommend perseverance&mdash;one of the tenets of Taekwon-Do... But in this case, don't bother: this URL just doesn't lead anywhere!</p>
+							<p><em>Ouch!</em> The board didn't break... We'd usually recommend perseverance, one of the tenets of Taekwon-Do, but it won't help you here: this URL just doesn't lead anywhere!</p>
 						</div>
 					</section>
 		    	</div>
