9583436994987ca2962908c2258b489835f94298
axelboc <axelboc@gmail.com>
2015-05-17 18:46:43 +1000

    feat: option to include errors when printing result

--- src/core/core.php
+++ src/core/core.php
@@ -39,7 +39,7 @@ function __autoload($className) {
  * Print the result of a form submission.
  * @param {String} $feature - the feature for which to print the results
  */
-function printResult($feature = null) {
+function printResult($feature = null, $includeErrors = false) {
 	// If no result or message to display, return;
 	if (!isset($_SESSION['result'])) {
 		return;
@@ -50,10 +50,20 @@ function printResult($feature = null) {
 	$localMatch = $feature !== null && isset($_SESSION['feature']) && $feature === $_SESSION['feature'];
 	
 	if ($globalMatch || $localMatch) {
-		echo '<div class="form-result-wrap lh" tabindex="-1">' . PHP_EOL .
-			 '	<p class="form-result form-result--' . $_SESSION['result']['type'] . '">' .
-			 		$_SESSION['result']['message'] . '</p>' . PHP_EOL .
-			 '</div>';
+		echo '<div class="form-result-wrap lh" tabindex="-1">';
+		echo '	<div class="form-result form-result--' . $_SESSION['result']['type'] . ' box">';
+		echo '		<p>' . $_SESSION['result']['message'] . '</p>';
+		
+		if ($includeErrors && isset($_SESSION['errors']) && count($_SESSION['errors']) > 0) {
+			echo '		<ul>';
+			foreach ($_SESSION['errors'] as $err) {
+				echo '			<li>' . $err . '</li>';
+			}
+			echo '		</ul>';
+		}
+		
+		echo '	</div>';
+		echo '</div>';
 		
 		unset($_SESSION['feature']); 
 		unset($_SESSION['result']); 
