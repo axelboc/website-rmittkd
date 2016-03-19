c5a6d4e8f12f7e3e192551f39a1831c98c27c469
Axel Bocciarelli <axelboc@gmail.com>
2016-03-19 11:57:45 +1100

    fix broken logo in 404

--- src/includes/sidebar.php
+++ src/includes/sidebar.php
@@ -4,12 +4,12 @@
 $merchItem = Merchandising::getRandom();
 
 // If an image URL is not provided, use the placeholder
-$merchImageSrc = $merchItem['merch-image'] ? $merchItem['merch-image'] : 'images/merchandising/placeholder.png';
+$merchImageSrc = $merchItem['merch-image'] ? $merchItem['merch-image'] : '/images/merchandising/placeholder.png';
 
 ?>
 <aside class="sidebar lay-side row backdrop" role="complementary">
 	<div class="sidebar-inner">
-		<a class="sidebar-rmit respimg-link" href="<?php echo CLUB_SHOP_URL; ?>" target="_blank"><img class="respimg-fit" src="images/rmit-link.png" width="150" height="35" alt="The club's page on RMIT Link"></a>
+		<a class="sidebar-rmit respimg-link" href="<?php echo CLUB_SHOP_URL; ?>" target="_blank"><img class="respimg-fit" src="/images/rmit-link.png" width="150" height="35" alt="The club's page on RMIT Link"></a>
 	</div>
 	<div class="sidebar-inner">
 		<h3>Club Shop</h3>
