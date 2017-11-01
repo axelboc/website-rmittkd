0239cd2296f1c5db41372e5ebad78b74a6eefc5f
Axel Bocciarelli <axelboc@gmail.com>
2017-11-01 20:48:28 +1100

    move netlify redirect to inline script

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,20 @@ import React from 'react'
 import Helmet from 'react-helmet'
 
 export default class IndexPage extends React.Component {
-  componentDidMount() {
-    if (!window.netlifyIdentity) return;
-    window.netlifyIdentity.on('init', user => {
-      if (user) return;
-      window.netlifyIdentity.on('login', () => {
-        document.location.href = '/admin/';
-      })
-    })
-  }
-
   render() {
     return (
       <div>Hello World</div>
+      <script dangerouslySetInnerHTML={{ __html:
+        `if (window.netlifyIdentity) {
+          window.netlifyIdentity.on("init", user => {
+            if (!user) {
+              window.netlifyIdentity.on("login", () => {
+                document.location.href = "/admin/";
+              });
+            }
+          });
+        }` }}
+      />
     )
   }
 }
