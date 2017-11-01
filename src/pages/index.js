1450c53f384759aa912f0cd601c206d8350dd97e
Axel Bocciarelli <axelboc@gmail.com>
2017-11-01 22:24:24 +1100

    remove GitHub Identity

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,6 @@ export default class IndexPage extends React.Component {
   render() {
     return (
       <div>Hello World</div>
-      <script dangerouslySetInnerHTML={{ __html:
-        `if (window.netlifyIdentity) {
-          window.netlifyIdentity.on("init", user => {
-            if (!user) {
-              window.netlifyIdentity.on("login", () => {
-                document.location.href = "/admin/";
-              });
-            }
-          });
-        }` }}
-      />
     )
   }
 }
