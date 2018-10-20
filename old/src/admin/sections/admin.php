acc0a641890fbd942804468dd0d1b03579fe8be6
Axel Bocciarelli <axelboc@gmail.com>
2018-10-20 20:11:18 +0200

    Remove `old` folder

--- old/src/admin/sections/admin.php
+++
@@ -1,8 +0,0 @@
-<section id="global" class="section section-admin--global row">
-	<h2 class="section-heading">Administration</h2>
-	<div class="section-content lh">
-		<?php Helpers::printResult(); ?>
-		<button id="help-btn" class="admin-btn help-btn button">Show help</button>
-		<p>Welcome to the administration console of the website. Please be aware that changes <strong>cannot be rolled back</strong>. Read the instructions carefully and do not hesitate to ask for advice.</p>
-	</div>
-</section>
