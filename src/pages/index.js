c8f8c01c023ff39d97e144fc871acc3c1857eb59
Axel Bocciarelli <axelboc@gmail.com>
2018-11-11 21:11:53 +0100

    Fix layout import paths

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 import PageMeta from '../components/PageMeta'
 import Banner from '../components/Banner/Banner'
 import Section from '../components/Section/Section'
